3e0ca708d8365882d6f46e8c7eab90887990a701
denoc1 <176614645+denoc1@users.noreply.github.com>
2025-09-10 03:02:51 +0000

    Template

---
+++ Lab2.java
@@ -0,0 +1,16 @@
+/*put your header code here*/
+
+
+public class Lab2{
+    public static void main (String[] args){
+
+        //write your variable declarations here
+
+
+
+        //write your statements here
+
+   
+    }
+
+}
